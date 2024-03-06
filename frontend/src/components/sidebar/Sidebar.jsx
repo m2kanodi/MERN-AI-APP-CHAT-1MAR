3e34184238c2e8a1c3a5dec416398dbf97f8bbe9
m2kanodi <123223377+m2kanodi@users.noreply.github.com>
2024-03-05 23:24:16 -0500

    2nd part ui-design done

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LogoutButton from './LogoutButton';
+import Conversations from './Conversations';
+import SearchInput from  './SearchInput';
+function Sidebar() {
+  return (
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
+			<SearchInput />
+			<div className='divider px-3'></div>
+			 <Conversations/>
+			 <LogoutButton/>
+		</div>
+  )
+}
+
+export default Sidebar;
